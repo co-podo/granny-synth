f8e49bc617b5f81aa5e17c937d011650e2252fbc
a-ludwig <adrian.ludwig@hs-augsburg.de>
2020-06-25 16:59:36 +0200

    created working communication between PD and python

--- deprecated/KY040/KY040.py
+++ deprecated/KY040/KY040.py
@@ -85,6 +85,19 @@ if __name__ == "__main__":
     CLOCKPIN1 = 22
     DATAPIN1 = 5
     SWITCHPIN1 = 6
+    
+    CLOCKPIN2 = 13 
+    DATAPIN2 = 19
+    SWITCHPIN2 = 26
+    
+    CLOCKPIN3 = 16
+    DATAPIN3 = 20
+    SWITCHPIN3 = 21
+     
+    CLOCKPIN4 = 23
+    DATAPIN4 = 24
+    SWITCHPIN4 = 25
+    
 
     def rotaryChange(direction, clockpin):
         print ("turned - " + str(direction))
@@ -97,11 +110,18 @@ if __name__ == "__main__":
     GPIO.setmode(GPIO.BCM)
     button0 = KY040(CLOCKPIN, DATAPIN, SWITCHPIN, rotaryChange, switchPressed)
     button1 = KY040(CLOCKPIN1, DATAPIN1, SWITCHPIN1, rotaryChange, switchPressed)
+    button2 = KY040(CLOCKPIN2, DATAPIN2, SWITCHPIN2, rotaryChange, switchPressed)
+    button3 = KY040(CLOCKPIN3, DATAPIN3, SWITCHPIN3, rotaryChange, switchPressed)
+    button4 = KY040(CLOCKPIN4, DATAPIN4, SWITCHPIN4, rotaryChange, switchPressed)
 
     print ('Launch switch monitor class.')
 
     button0.start()
     button1.start()
+    button2.start()
+    button3.start()
+    button4.start()
+    
     print ('Start program loop...')
     try:
         while True:
@@ -111,6 +131,9 @@ if __name__ == "__main__":
         print ('Stopping GPIO monitoring...')
         button0.stop()
         button1.stop()
+        button2.stop()
+        button3.stop()
+        button4.stop()
         GPIO.cleanup()
         print ('Program ended.')
 
